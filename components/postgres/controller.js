41cec3dd4e38eb79d92615f88fab789b2e8c303b
feliun <felipe.polo@gmail.com>
2018-02-21 12:44:44 +0100

    Getting single/multi family

--- components/postgres/controller.js
+++ components/postgres/controller.js
@@ -2,7 +2,11 @@ const debug = require('debug')('offers-controller');
 module.exports = (options) => {
 
     const start = ({ postgres, config, logger }, cb) => {
-        const getFamilies = () => {
+        const getFamilies = (id) => {
+            if (id) {
+                debug(`Getting data for family ${id}`);
+                return Promise.resolve({ id: 0 });
+            }
             debug('Getting all families...');
             return Promise.resolve([{ id: 0 }, { id: 1 }]);
         };
--- components/routes/rest-api-routes.js
+++ components/routes/rest-api-routes.js
@@ -4,12 +4,17 @@ const sequential = R.reduce((chain, promise) => chain.then(promise), Promise.res
 module.exports = () => {
 
     const start = ({ controller, app }, cb) => {
-        app.get('/api/v1/families', (req, res) => {
-            sequential([
-                () => controller.getFamilies()
-            ])
-            .then((families) => res.json(families))
-            .catch((err) => res.json(err));
+
+        const getData = (sequence) => (res) =>
+            sequential(sequence)
+                .then((response) => res.json(response))
+                .catch((err) => res.status(401).json(err));
+
+        app.get([ '/api/v1/families', '/api/v1/families/:family' ], (req, res) => {
+            const { family } = req.params;
+            getData([
+                () => controller.getFamilies(family)
+            ])(res);
         });
         cb();
     };
